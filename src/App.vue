6c763f891cf5a494ca0256847d45e0748fb33d60
Carlos Cuamatzin Hernández <kuamatzin@gmail.com>
2022-11-09 23:55:28 -0600

    Winners

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,10 @@
 
       <div v-else>
         <div class="desktop">
-          <img src="./assets/images/wait_winners.jpeg" width="100%">
+          <img src="./assets/images/winners.jpeg" width="100%">
         </div>
         <div class="mobile">
-          <img src="./assets/images/wait_winners.jpeg" width="100%">
+          <img src="./assets/images/winners_mobile.jpeg" width="100%">
         </div>
       </div>
     </div>
